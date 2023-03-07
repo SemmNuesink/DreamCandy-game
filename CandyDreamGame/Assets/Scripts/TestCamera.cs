e57fd05aa7841622d6e64f81e0afd9459d929fe6
ArthurVanIttersum <124685165+ArthurVanIttersum@users.noreply.github.com>
2023-03-07 15:48:27 +0100

    camera controler gedebugd en opgeschoond

--- CandyDreamGame/Assets/Scripts/TestCamera.cs
+++ CandyDreamGame/Assets/Scripts/TestCamera.cs
@@ -10,61 +10,93 @@ using UnityEngine.UIElements;
 
 public class TestCamera : MonoBehaviour
 {
-    public GameObject objectCameraljdlf;
+    public GameObject CameraThing;
+    public float playerMovementSpeed;
+    public float cameraSensitivity;
+
     public float x1;
     public float x2;
-    public float x3;
-    
+
+    public float h;
+    public float v;
+
+    public Vector3 newPlayerPos;
+    public Vector3 newCameraPos;
+
     public Vector3 cameraRotation;
     public Vector3 thisPos;
-    public Vector3 thisRot;
+    
     
     public float cameraOffset;
-    public Vector3 newCameraPos;
-    public float cameraSensitivity;
-    public quaternion vector3ToQuaternion;
+    
+    
+    
     public RaycastHit hit;
-    public float t;
+    public float neededZoom;
     public Vector3 newCameraZoom;
-
+    
 
     // Update is called once per frame
     void Update()
     {
-        thisPos = this.transform.position;
-        x1 = Input.GetAxis("Mouse X");
-
-        x2 += x1 * cameraSensitivity * Time.deltaTime / 180 * math.PI;
         
+        x1 = Input.GetAxis("Mouse X");
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        x2 += x1 * cameraSensitivity * Time.deltaTime / 180 * math.PI; ;
+        
+        newPlayerPos.x = h;
+        newPlayerPos.z = v;
+
+        transform.Translate(newPlayerPos * Time.deltaTime * playerMovementSpeed);
+
+        if (!((h == 0) && (v == 0)))
+        {
+            transform.rotation = CameraThing.transform.rotation;
+        }
+        
+
+        thisPos = this.transform.position;
+
         newCameraPos = thisPos;
         newCameraPos.z -= math.cos(x2) * cameraOffset;
-        newCameraPos.y += 1f;
+        newCameraPos.y += 0f;
         newCameraPos.x -= math.sin(x2) * cameraOffset;
 
         cameraRotation.y = x1 * cameraSensitivity * Time.deltaTime;
 
-        objectCameraljdlf.transform.Rotate(cameraRotation);
+        CameraThing.transform.Rotate(cameraRotation);
         do
         {
-            newCameraZoom = Vector3.Lerp(newCameraPos,thisPos, t);
-            if (Physics.Raycast(newCameraZoom, objectCameraljdlf.transform.forward, out hit, 5))
+            newCameraZoom = Vector3.Lerp(newCameraPos, thisPos, neededZoom);
+            if (Physics.Raycast(newCameraZoom, CameraThing.transform.forward, out hit, 5))
             {
-                t = 0;
-                break;
+                if (hit.collider.gameObject == this.gameObject)
+                {
+                    neededZoom = 0;
+                    break;
+                }
+                else
+                {
+                    neededZoom += 0.01f;
+                    
+                }
+                if (neededZoom > 1)
+                {
+                    neededZoom = 1;
+                    print("Oei! de player kan niet in beeld worden gebracht");
+                    break;
+                }
             }
             else
             {
-                t += 0.1f;
-            }
-            if (t > 1)
-            {
-                t = 1;
                 print("Oeps! de camera is niet gericht op de player");
                 break;
             }
         } while (true);
 
-        objectCameraljdlf.transform.position = newCameraPos;
+        CameraThing.transform.position = newCameraZoom;
 
     }
 }
