4e2f9ae21cc8fa9156f6f08e19976d09a4129340
ArthurVanIttersum <124685165+ArthurVanIttersum@users.noreply.github.com>
2023-03-10 16:01:41 +0100

    cameraHight

--- CandyDreamGame/Assets/Scripts/TestCamera.cs
+++ CandyDreamGame/Assets/Scripts/TestCamera.cs
@@ -12,10 +12,13 @@ public class TestCamera : MonoBehaviour
 {
     public GameObject CameraThing;
     public float playerMovementSpeed;
-    public float cameraSensitivity;
+    public float cameraSensitivityv;
+    public float cameraSensitivityh;
 
     public float x1;
     public float x2;
+    public float y1;
+    public float y2;
 
     public float h;
     public float v;
@@ -41,13 +44,25 @@ public class TestCamera : MonoBehaviour
     {
         
         x1 = Input.GetAxis("Mouse X");
+        y1 = Input.GetAxis("Mouse Y");
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
-        x2 += x1 * cameraSensitivity * Time.deltaTime / 180 * math.PI; ;
+        x2 += x1 * cameraSensitivityh * Time.deltaTime / 180 * math.PI; ;
         
         newPlayerPos.x = h;
         newPlayerPos.z = v;
+
+        y2 += y1 * cameraSensitivityv;
+
+        if (y2 > 5)
+        {
+            y2= 5;
+        }
+        else if (y2 < 1)
+        {
+            y2= 1f;
+        }  
 
         transform.Translate(newPlayerPos * Time.deltaTime * playerMovementSpeed);
 
@@ -56,21 +71,20 @@ public class TestCamera : MonoBehaviour
             transform.rotation = CameraThing.transform.rotation;
         }
         
-
         thisPos = this.transform.position;
 
         newCameraPos = thisPos;
-        newCameraPos.z -= math.cos(x2) * cameraOffset;
-        newCameraPos.y += 0f;
-        newCameraPos.x -= math.sin(x2) * cameraOffset;
+        newCameraPos.z -= math.cos(x2) * y2;
+        newCameraPos.y += y2 * 0.5f;
+        newCameraPos.x -= math.sin(x2) * y2;
 
-        cameraRotation.y = x1 * cameraSensitivity * Time.deltaTime;
+        cameraRotation.y = x1 * cameraSensitivityh * Time.deltaTime;
 
         CameraThing.transform.Rotate(cameraRotation);
         do
         {
             newCameraZoom = Vector3.Lerp(newCameraPos, thisPos, neededZoom);
-            if (Physics.Raycast(newCameraZoom, CameraThing.transform.forward, out hit, 5))
+            if (Physics.Raycast(newCameraZoom, CameraThing.transform.forward, out hit, 10f))
             {
                 if (hit.collider.gameObject == this.gameObject)
                 {
@@ -80,11 +94,10 @@ public class TestCamera : MonoBehaviour
                 else
                 {
                     neededZoom += 0.01f;
-                    
                 }
-                if (neededZoom > 1)
+                if (neededZoom > 0.9f)
                 {
-                    neededZoom = 1;
+                    neededZoom = 0.9f;
                     print("Oei! de player kan niet in beeld worden gebracht");
                     break;
                 }
